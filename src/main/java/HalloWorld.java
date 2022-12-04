0a31f1a9eab9e552a792db30d9b6c3d9888608de
Kenan <Kenan@informatik.de>
2022-12-04 09:17:06 +0100

    dritte line

--- src/main/java/HalloWorld.java
+++ src/main/java/HalloWorld.java
@@ -2,5 +2,6 @@ public class HalloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo World");
         System.out.println("Hallo World2");
+        System.out.println("Hello World 3");
     }
 }
